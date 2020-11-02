0e72b795362ae7ae56d72201615526f38e525bd3
taylorbett <taylor.bett@hotmail.com>
2020-11-02 19:37:38 +1100

    feat: added auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import Amplify from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
